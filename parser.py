16c9282aabe753ee0d6b893573703dcea5375b2c
veljko <veljko973@gmail.com>
2020-09-17 18:47:40 +0200

    drugi dan

--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+from docxtpl import DocxTemplate, Listing
 import re
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 from xml.etree import ElementTree
@@ -21,12 +22,12 @@ def extractVariables(allVariables,variables):
     """
     
     splitedVariables = re.split(r'[\s\[\]+\-\\*/\d]+',variables)
+    print(splitedVariables)
     for variable in splitedVariables:
-        if not variable.isdigit() and variable != '' and variable != 'true' and variable != 'false':
+        if variable.islower() and variable != '' and variable != 'true' and variable != 'false':
             if not variable in allVariables:
                 allVariables[variable] = ''
     return allVariables
-
 
 
 
@@ -46,6 +47,8 @@ def shuffleNames(allVariables):
         allVariables[variable]=randomLetter
     
     return allVariables
+
+
 
 currentScope = Element("program")
 allScopes = [currentScope]
@@ -76,9 +79,9 @@ for line in inpupFile:
         allVariables = extractVariables(allVariables,matched.group('endVar'))
 
         
-        foundBegin = re.search(r'begin',line) is not None
-        startedLoop = not foundBegin
-        numberOfLoopLines = 0
+        #foundBegin = re.search(r'begin',line) is not None
+        #startedLoop = not foundBegin
+        #numberOfLoopLines = 0
 
         print(prettify(top))
 
@@ -103,9 +106,9 @@ for line in inpupFile:
 
     #search for begin
     matched = re.search(r'[b|B]egin',line)
-    if matched is not None:
-        startedLoop = True
-        foundBegin = True
+    #if matched is not None:
+        #startedLoop = True
+        #foundBegin = True
     
 
     #search for repeat
@@ -118,8 +121,8 @@ for line in inpupFile:
         allScopes.append(top)
         currentScope = top
 
-        foundBegin = True
-        startedLoop = True
+        #foundBegin = True
+        #startedLoop = True
         numberOfLoopLines = 2
 
     #search for until
@@ -140,11 +143,28 @@ for line in inpupFile:
         currentScope = allScopes.pop()
 
 
+    #search for while loop
+    matched = re.search(r'[wW]hile( +|)(?P<condition>.+?(?=\sdo))',line)
+    if matched is not None:
+        top = SubElement(currentScope,"loop",{'type':'while', 'condition':matched.group('condition'), 'depth': str(depth)})
+        depth += 1
+        allScopes.append(top)
+        currentScope = top
+
+
+    #search for if statement
+    matched = re.search(r'[iI]f( +|)(?P<condition>(.+?(?=\sthen)))',line)
+    if matched is not None:
+        top = SubElement(currentScope,"if",{'condition':matched.group('condition'), 'depth': str(depth)})
+        depth += 1
+        allScopes.append(top)
+        currentScope = top
+
     #handeling of one line for loop
     if not foundBegin and numberOfLoopLines == 1:
         print(f"3end of scope for {currentScope}")
         currentScope = allScopes.pop()
-        startedLoop = False
+        #startedLoop = False
         
     if startedLoop:
         numberOfLoopLines+=1
@@ -162,21 +182,42 @@ tree = ElementTree.ElementTree(allScopes[0])
 compiledRegexLetters = dict()
 
 for letter in allVariables:
-    compiledRegexLetters[letter] = re.compile(letter)
+    compiledRegexLetters[letter] = re.compile('\b(?!false|true)\b' + letter)
 
 for elem in tree.iter():
     if elem.tag == 'instruction':
-        elem.attrib['var1']=allVariables[elem.attrib['var1']]
         for var in allVariables:
-            elem.attrib['var2'] = re.sub(var,allVariables[var],elem.attrib['var2'])
+            elem.attrib['var1'] = re.sub(var,allVariables[var],elem.attrib['var1'])
+            if elem.attrib['var2'] != 'true' and elem.attrib['var2'] != 'false':
+                elem.attrib['var2'] = re.sub(var,allVariables[var],elem.attrib['var2'])
+            else:
+                elem.attrib['var2'] = re.sub('true','1',elem.attrib['var2'])
+                elem.attrib['var2'] = re.sub('false','0',elem.attrib['var2'])
         
     if elem.tag == 'loop':
-        elem.attrib['iterVar']=allVariables[elem.attrib['iterVar']]
-        for var in allVariables:
-            elem.attrib['endVar'] = re.sub(var,allVariables[var],elem.attrib['endVar'])
-        if(elem.attrib['type'] == 'for'):
+        if elem.attrib['type'] != 'while':
             for var in allVariables:
-                elem.attrib['iterVal'] = re.sub(var,allVariables[var],elem.attrib['iterVal'])
+                elem.attrib['endVar'] = re.sub(var,allVariables[var],elem.attrib['endVar'])
+                elem.attrib['iterVar']=re.sub(var,allVariables[var],elem.attrib['iterVar'])
+            if(elem.attrib['type'] == 'for'):
+                for var in allVariables:
+                    elem.attrib['iterVal'] = re.sub(var,allVariables[var],elem.attrib['iterVal'])
+        else:
+            elem.attrib['condition'] = re.sub('and','&&',elem.attrib['condition'])
+            elem.attrib['condition'] = re.sub('or','||',elem.attrib['condition'])
+            elem.attrib['condition'] = re.sub(' =',' ==',elem.attrib['condition'])
+            elem.attrib['condition'] = re.sub('<>','!=',elem.attrib['condition'])
+            for var in allVariables:
+                elem.attrib['condition'] = re.sub(var,allVariables[var],elem.attrib['condition'])
+
+
+    if elem.tag == 'if':
+        elem.attrib['condition'] = re.sub('and','&&',elem.attrib['condition'])
+        elem.attrib['condition'] = re.sub('or','||',elem.attrib['condition'])
+        elem.attrib['condition'] = re.sub(' =',' ==',elem.attrib['condition'])
+        elem.attrib['condition'] = re.sub('<>','!=',elem.attrib['condition'])
+        for var in allVariables:
+                elem.attrib['condition'] = re.sub(var,allVariables[var],elem.attrib['condition'])
             
 
 print(prettify(tree.getroot()))
@@ -184,31 +225,74 @@ print(prettify(tree.getroot()))
 lastPadding = 0
 curlyBracket = ''
 padding = 0
+pandingBrackets = []
+doWhilelines = dict()
 
+code_snippet = ''
+
+#printing in c
 for elem in tree.iter():
     lastPadding = padding
-    cString = ''
-    if elem.tag == 'instruction' or elem.tag == 'instruction':
+    if elem.tag == 'instruction' or elem.tag == 'instruction' or elem.tag == 'if':
         padding = int(elem.attrib['depth'])
-        print(f"padding: {padding} ; lastPadding: {lastPadding}")
+
+
         if padding > lastPadding:
             curlyBracket = '{'
+            print( '\t' * (padding - 1) + curlyBracket)
+            code_snippet += '\t' * (padding - 1) + curlyBracket + '\r\n'
+            pandingBrackets.append('}')
         elif padding < lastPadding:
+            if(doWhilelines[padding]):
+                print('\t' * padding + doWhilelines[padding])
+                code_snippet += '\t' * padding + doWhilelines[padding] + '\r\n'
             curlyBracket = '}'
         else:
             curlyBracket = ''
-        print(curlyBracket)
-        cString = ''
-        for i in range(padding):
-            cString += '\t'
+        
+    #instruction
     if elem.tag == 'instruction':
         
-        print( cString + f"{elem.attrib['var1']} = {elem.attrib['var2']};") 
+        print( '\t' * padding + f"{elem.attrib['var1']} = {elem.attrib['var2']};")
+        code_snippet += '\t' * padding + f"{elem.attrib['var1']} = {elem.attrib['var2']};" +  '\r\n' 
     
     if elem.tag == 'loop':
+        #for loop
         if elem.attrib['type'] == 'for':
-            print( cString + f"for({elem.attrib['iterVar']} = {elem.attrib['iterVal']};{elem.attrib['iterVar']} { '<' if elem.attrib['inc'] == '++' else '>='} {elem.attrib['endVar']};{elem.attrib['iterVar']}{elem.attrib['inc']}) {curlyBracket}")
+            print( '\t' * padding + f"for({elem.attrib['iterVar']} = {elem.attrib['iterVal']};{elem.attrib['iterVar']} { '<' if elem.attrib['inc'] == '++' else '>='} {elem.attrib['endVar']};{elem.attrib['iterVar']}{elem.attrib['inc']})")
+            code_snippet += '\t' * padding + f"for({elem.attrib['iterVar']} = {elem.attrib['iterVal']};{elem.attrib['iterVar']} { '<' if elem.attrib['inc'] == '++' else '>='} {elem.attrib['endVar']};{elem.attrib['iterVar']}{elem.attrib['inc']})" +  '\r\n' 
+
+        #repeat loop
+        if elem.attrib['type'] == 'repeat':
+            print(padding * '\t' + "do")
+            code_snippet += padding * '\t' + "do" +  '\r\n' 
+            doWhilelines[padding] = f"}} while({elem.attrib['iterVar']} {elem.attrib['op']} {elem.attrib['endVar']});"
+            pandingBrackets.pop()
+        #while loop
+        if elem.attrib['type'] == 'while':
+            print( '\t' * padding + f"while({elem.attrib['condition']})")
+            code_snippet += '\t' * padding + f"while({elem.attrib['condition']})" +  '\r\n' 
+
+    if elem.tag == 'if':
+        print( '\t' * padding + f"if({elem.attrib['condition']})")
+        code_snippet += '\t' * padding + f"if({elem.attrib['condition']})" +  '\r\n' 
 
 
     if curlyBracket == '}':
-        print('}')
+        if(doWhilelines[padding]):
+            doWhilelines.pop(padding)
+        else:
+            print( '\t' * padding + '}')
+            code_snippet += '\t' * padding + '}' +  '\r\n' 
+            pandingBrackets.pop()
+
+for idx,bracket in enumerate(pandingBrackets):
+    print((len(pandingBrackets) - 1 - idx) * '\t' + '}')
+    code_snippet += (len(pandingBrackets) - 1 - idx) * '\t' + '}' +  '\r\n' 
+
+
+
+doc = DocxTemplate("templates/one_program.docx")
+context = { 'code_snippet' : Listing(code_snippet) }
+doc.render(context)
+doc.save("generated_doc.docx")
